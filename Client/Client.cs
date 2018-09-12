bafca8642fbfed978134bc990b6680d136be79f8
Sebastián Caraballo <sebastiancaruy1@gmail.com>
2018-09-12 17:58:29 -0300

    Third commit
    Los clientes se pueden conectar al servidor. El servidor le indica conexión exitosa cuando un cliente se conecta. El cliente le puede enviar comandos al servidor.

--- Client/Client.cs
+++ Client/Client.cs
@@ -22,14 +22,68 @@ namespace Client
             int n = Int32.Parse(Console.ReadLine());
             client.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), n));
             client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6000));
-            Console.WriteLine("Conectado al servidor.");
 
+            var t1 = new Thread(() => Receive(client));
+            var t2 = new Thread(() => Send(client));
+            t1.Start();
+            t2.Start();
+
+        }
+
+        static void Receive(Socket client)
+        {
             while (true)
             {
-                string cmd = Console.ReadLine();
 
+                var pos = 0;
+                var lengthInBytes = new byte[4];
+                var i = 0;
+                while (i < 4)
+                {
+                    i += client.Receive(lengthInBytes, i, 4 - i, SocketFlags.None);
+                }
+                int length = BitConverter.ToInt32(lengthInBytes, 0);
+                var msgBytes = new byte[length];
+                while (pos < length)
+                {
+                    var recieved = client.Receive(msgBytes, pos, length - pos, SocketFlags.None);
+                    if (recieved == 0) throw new SocketException();
+                    pos += recieved;
+                }
+
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(msgBytes));
             }
-            
+        }
+
+        static void Send(Socket client)
+        {
+            while (true)
+            {
+                var msg = Console.ReadLine();
+                SendBitsLength(client, msg);
+
+                var byteMsg = System.Text.Encoding.ASCII.GetBytes(msg); //lo convierte a un array de bytes
+                var length = byteMsg.Length;
+                var pos = 0;
+
+                while (pos < length)
+                {
+                    var sent = client.Send(byteMsg, pos, length - pos, SocketFlags.None);
+                    if (sent == 0) throw new SocketException();
+                    pos += sent;
+                }
+            }
+        }
+
+        private static void SendBitsLength(Socket client, string msg)
+        {
+            var messaageLengthInInt = msg.Length;
+            var messageLengthInBit = BitConverter.GetBytes(messaageLengthInInt);
+            var i = 0;
+            while (i < 4)
+            {
+                i += client.Send(messageLengthInBit, i, 4 - i, SocketFlags.None);
+            }
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -24,8 +24,63 @@ namespace Obligatorio
             while (true)
             {
                 var client = server.Accept();
-                var thread = new Thread(() => Game.Welcome(client));
-                thread.Start();
+                var t1 = new Thread(() => Receive(client));
+                var t2 = new Thread(() => Send(client, "Cliente conectado."));
+                t1.Start();
+                t2.Start();
+            }
+        }
+
+
+        static void Receive(Socket client)
+        {
+            while (true)
+            {
+                var pos = 0;
+                var lengthInBytes = new byte[4];
+                var i = 0;
+                while (i < 4)
+                {
+                    i += client.Receive(lengthInBytes, i, 4 - i, SocketFlags.None);
+                }
+                int length = BitConverter.ToInt32(lengthInBytes, 0);
+                var msgBytes = new byte[length];
+                while (pos < length)
+                {
+                    var recieved = client.Receive(msgBytes, pos, length - pos, SocketFlags.None);
+                    if (recieved == 0) throw new SocketException();
+                    pos += recieved;
+                }
+
+                Console.WriteLine(System.Text.Encoding.ASCII.GetString(msgBytes));
+            }
+        }
+
+        static void Send(Socket client, string message = "")
+        {
+            var msg = message;
+            SendBitsLength(client, msg);
+
+            var byteMsg = System.Text.Encoding.ASCII.GetBytes(msg); //lo convierte a un array de bytes
+            var length = byteMsg.Length;
+            var pos = 0;
+
+            while (pos < length)
+            {
+                var sent = client.Send(byteMsg, pos, length - pos, SocketFlags.None);
+                if (sent == 0) throw new SocketException();
+                pos += sent;
+            }
+        }
+
+        private static void SendBitsLength(Socket client, string msg)
+        {
+            var messaageLengthInInt = msg.Length;
+            var messageLengthInBit = BitConverter.GetBytes(messaageLengthInInt);
+            var i = 0;
+            while (i < 4)
+            {
+                i += client.Send(messageLengthInBit, i, 4 - i, SocketFlags.None);
             }
         }
     }
