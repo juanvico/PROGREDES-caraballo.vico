f01ce18263e7822624af599d80e050c72c707633
sebastiancaraballo <sebastiancaruy1@gmail.com>
2018-09-12 16:59:04 -0300

    Initial commit

---
+++ Server/Logic/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public class Game
+    {
+        public static void Welcome(Socket client)
+        {
+            Console.WriteLine("Nuevo jugador conectado.");
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,33 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Obligatorio
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            var server = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+            server.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6000));
+            server.Listen(25);
+            while (true)
+            {
+                var client = server.Accept();
+                var thread = new Thread(() => Game.Welcome(client));
+                thread.Start();
+
+            }
+        }
+    }
+}
